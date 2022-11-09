0492c2fa65413010369c3e7bd6a6cb7cedf9cb4d
n3integration <n3integration@gmail.com>
2022-11-08 22:07:35 -0500

    improvements to pattern construction

--- trie.go
+++ trie.go
@@ -13,12 +13,12 @@ type Trie struct {
 	nodes map[rune]*Trie
 }
 
-// NewTrie initializes an empty trie structure
+// NewTrie initializes an empty trie data structure
 func NewTrie() *Trie {
 	return &Trie{}
 }
 
-// Add saves
+// Add stores a string within the underlying data structure
 func (t *Trie) Add(text string) {
 	current := t
 	for _, r := range text {
@@ -40,38 +40,44 @@ func (t *Trie) ToRegex() (*regexp.Regexp, error) {
 		return nil, errors.New("trie is empty")
 	}
 
-	regex := t.regex()
-	return regexp.Compile(fmt.Sprintf("^%s$", regex))
+	rePattern := t.Pattern()
+	return regexp.Compile(fmt.Sprintf("^%s$", rePattern))
 }
 
-func (t *Trie) regex() string {
+// Pattern exposes the trie's regular expression fragment as a string
+func (t *Trie) Pattern() string {
 	if len(t.nodes) == 0 {
 		return ""
 	}
 	if len(t.nodes) == 1 {
 		key := t.keys()[0]
-		return fmt.Sprintf("%s%s", string(key), t.nodes[key].regex())
+		return fmt.Sprintf("%s%s", regexp.QuoteMeta(string(key)), t.nodes[key].Pattern())
 	}
 
 	sequences := make([]string, 0)
 	for key, trie := range t.nodes {
-		sequences = append(sequences, fmt.Sprintf("%s%s", string(key), trie.regex()))
+		sequences = append(sequences, fmt.Sprintf("%s%s", regexp.QuoteMeta(string(key)), trie.Pattern()))
 	}
 
 	if len(sequences) == 1 {
 		return sequences[0]
 	}
-	if len(sequences) == len(strings.Join(sequences, "")) {
-		return fmt.Sprintf("[%s]", strings.Join(sequences, ""))
+	subsequence := strings.Join(sequences, "")
+	if len(sequences) == len(subsequence) {
+		return fmt.Sprintf("[%s]", subsequence)
 	}
 	sort.Slice(sequences, func(i, j int) bool {
-		return len(sequences[i]) < len(sequences[j])
+		diff := strings.Compare(sequences[i], sequences[j])
+		if diff == 0 {
+			return len(sequences[i]) < len(sequences[j])
+		}
+		return diff < 0
 	})
 	return fmt.Sprintf("(?:%s)", strings.Join(sequences, "|"))
 }
 
 func (t *Trie) keys() []rune {
-	keys := make([]rune, 0)
+	var keys []rune
 	for key := range t.nodes {
 		keys = append(keys, key)
 	}
